540aa08842a7965d3054946f8221ee4b166d010c
Vieira Lisboa <vieira.lisboa@yahoo.com>
2013-12-05 11:08:05 +0000

    Implements section navigation using jQuery

--- js/index.js
+++ js/index.js
@@ -3,3 +3,23 @@
 if(console) console.log("TWeb TP2 Pronto.");
 
 if(!window.jQuery) throw "jQuery não Encontrado!";
+
+$('#main-nav li').click(function(){
+	// quando digo seleccionar/deseleccionar
+	// quero dizer adicionar/remover a classe 'selected'
+	
+	// this é o <li> do menu 
+	$(this)
+		// selecciona o seu parente
+		.addClass('selected')
+		// deseleciona o seu irmão selecionado 
+		.siblings().removeClass('selected');
+	
+	// deseleciona a secção activa
+	$('#content section.selected').removeClass('selected');
+	
+	// seleciona a secção pedida
+	var id = $(this).find('a').attr('href');
+	$(id).addClass('selected');
+});
+
