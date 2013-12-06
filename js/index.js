45f549f26be41a5c63d8a6fb9d70d033346d3a05
Vieira Lisboa <vieira.lisboa@yahoo.com>
2013-12-06 12:02:17 +0000

    Implements content loading using jQuery

--- js/index.js
+++ js/index.js
@@ -4,22 +4,8 @@ if(console) console.log("TWeb TP2 Pronto.");
 
 if(!window.jQuery) throw "jQuery não Encontrado!";
 
-$('#main-nav li').click(function(){
-	// quando digo seleccionar/deseleccionar
-	// quero dizer adicionar/remover a classe 'selected'
-	
-	// this é o <li> do menu 
-	$(this)
-		// selecciona o seu parente
-		.addClass('selected')
-		// deseleciona o seu irmão selecionado 
-		.siblings().removeClass('selected');
-	
-	// deseleciona a secção activa
-	$('#content section.selected').removeClass('selected');
-	
-	// seleciona a secção pedida
-	var id = $(this).find('a').attr('href');
-	$(id).addClass('selected');
-});
-
+$("#header").load("/TWeb_TP2/content/header.html");
+$("#nav").load("/TWeb_TP2/content/nav.html");
+$("#footer").load("/TWeb_TP2/content/footer.html");
+$("#content").load("/TWeb_TP2/content/section.home.html");
+//$("aside").get(0).load("/TWeb_TP2/content/aside.html");
