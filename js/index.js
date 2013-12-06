71e08dd7bf903aaef4d57e3c913728166f928c3b
Vieira Lisboa <vieira.lisboa@yahoo.com>
2013-12-06 13:35:14 +0000

    Modifies content navigation to use the hashchange event

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 //JavaScript
 
-if(console) console.log("TWeb TP2 Pronto.");
-
-if(!window.jQuery) throw "jQuery não Encontrado!";
-
 $("#header").load("/TWeb_TP2/content/header.html");
+
 $("#nav").load("/TWeb_TP2/content/nav.html");
+
 $("#footer").load("/TWeb_TP2/content/footer.html");
+
 $("#content").load("/TWeb_TP2/content/section.home.html");
+
 //$("aside").get(0).load("/TWeb_TP2/content/aside.html");
