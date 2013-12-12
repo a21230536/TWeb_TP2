c0cb962170415ea17213ab43cd30edc5bed27c99
a21230536 <a21230536@alunos.isec.pt>
2013-12-12 21:55:18 +0000

    Adds the JavaScript object TP2 with the project's functions

--- js/index.js
+++ js/index.js
@@ -1,11 +1,46 @@
 //JavaScript
 
-$("#header").load("/TWeb_TP2/content/header.html");
+window.TP2 = {
+    header: function(url){
+        this._load("#header", url);
+    },
+    footer: function(url){
+        this._load("#footer", url);
+    },
+    nav: function(url){
+        this._load("#nav", url);
+    },
+    section: function(section){
+        section = section || location.hash;
+        TP2._load("#section",
+            "/TWeb_TP2/content/section."+ section.replace("#", "") +".html");
+    },
+    aside: function(url){
+        this._load("#aside", url);
+    },
+    article: function(article, cssClass){
+        cssClass = cssClass || article + " selected";
+        $('<article class="' + cssClass + '">')
+            .load('content/article.'+article+'.html')
+            .appendTo("div.body");
+    },
+    menuItem: {
+        click: function(){
+            TP2.menuItem.toggle($(this));
+        },
+        toggle: function($el){
+            $el = $el || $("a[href=" + location.hash + "]");
+            var $menuItem = $el.parent();
+            if($menuItem.hasClass('selected')) return;
+            $menuItem.addClass('selected').siblings().removeClass('selected');
+        }
+    },
+    _load: function(el, url){
+        $(el).load(url);
+    }
+};
 
-$("#nav").load("/TWeb_TP2/content/nav.html");
-
-$("#footer").load("/TWeb_TP2/content/footer.html");
-
-$("#section").load("/TWeb_TP2/content/section.home.html");
-
-$("aside").load("/TWeb_TP2/content/aside.html");
+window.onhashchange = function(){
+    TP2.menuItem.toggle();
+    TP2.section();
+};
